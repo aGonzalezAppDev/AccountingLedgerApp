6908dd5d1ca3fda8285194b7b4c342ad727b39aa
aGonzalezAppDev <agonzalez@appdev.yearup.org>
2023-05-01 16:00:29 -0400

    Third commit

--- src/main/java/com/learntocode/AccountingLedgerApp.java
+++ src/main/java/com/learntocode/AccountingLedgerApp.java
@@ -39,8 +39,7 @@ public class AccountingLedgerApp {
                  break;
              case "L":
                  // Display Ledger Screen and sub methods
-
-
+                 displayLedgerScreen(myScanner);
                  break;
              case "X":
                  System.out.println("Exiting Application!!");
@@ -92,6 +91,7 @@ public class AccountingLedgerApp {
         }
     }
 
+    // make method for making a payment and adding it to the csv file
     private static void makePayment(Scanner myScanner){
         try {
             BufferedWriter bufWriter = new BufferedWriter(new FileWriter("transactions.csv"));
@@ -107,8 +107,137 @@ public class AccountingLedgerApp {
     }
 
 
+    private static void displayLedgerScreen(Scanner myScanner){
+        String ledgerInput = "";
+        while (!ledgerInput.equalsIgnoreCase("H")){
+            //Display Ledger Screen
+            System.out.println("---------------");
+            System.out.println("Ledger Screen");
+            System.out.println("---------------");
+            System.out.println();
+            System.out.println("A) All - Display all entries.");
+            System.out.println("D) Deposits - Display only deposits into account");
+            System.out.println("P) Payments - Display only payments"); // negative entries
+            System.out.println("R) Reports - Menu with pre-defined reports or run custom search feature");
+            System.out.println("H) Home - go back to home screen");
+            System.out.println("----------------------------------------------------------------------------");
+            ledgerInput = myScanner.nextLine();
+            // create switch for Ledger Screen options
+            switch(ledgerInput){
+                case "A":
+                    // display all entries
+                    displayAllEntries();
+                    break;
+                case "D":
+                    // display deposits only
+                    displayDepositsOnly();
+                    break;
+                case "P":
+                    // display payments only
+                    displayPaymentsOnly();
+                    break;
+                case "R":
+                    // show reports and do other pre-defined reports and custom search
 
-    
+                    break;
+                case "H":
+                    // go back to home screen
+                    System.out.println("Going back to Home Screen!");
+                    break;
+                default:
+                    System.out.println("Invalid input!! Please use on of the above inputs!");
+                    break;
+            }
+
+        }
+    }
+
+    public static void displayAllEntries(){
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("transactions.csv"));
+
+            String input;
+            // read all the data in the file
+            while (((input = reader.readLine()) != null)) {
+                System.out.println(input);
+            }
+            reader.close();
+            } catch(FileNotFoundException e){
+                throw new RuntimeException(e);
+            } catch(IOException e){
+                throw new RuntimeException(e);
+            }
+        }
+
+
+
+    public static void displayDepositsOnly(){
+        ArrayList<Ledger> ledger = new ArrayList<>();
+        String line;
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("transactions.csv"));
+            while((line = reader.readLine()) != null) {
+                String [] parts = line.split("\\|");
+                String date = parts[0];
+                String time = parts[1];
+                String description = parts[2];
+                String vendor = parts[3];
+                double amount = Double.parseDouble(parts[4]);
+                for (Ledger deposit : ledger) {
+                    if(amount > 0) {
+                        System.out.printf("%s|%s|%s|%s|$%.2f%n",
+                                deposit.getDate(), deposit.getTime(), deposit.getDescription(),deposit.getVendor(),deposit.getAmount());
+                    }
+                }
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    public static void displayPaymentsOnly(){
+        ArrayList<Ledger> ledger = new ArrayList<>();
+        String line;
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("transactions.csv"));
+            while((line = reader.readLine()) != null) {
+                String [] parts = line.split("\\|");
+                String date = parts[0];
+                String time = parts[1];
+                String description = parts[2];
+                String vendor = parts[3];
+                double amount = Double.parseDouble(parts[4]);
+                for (Ledger deposit : ledger) {
+                    if(amount > 0) {
+                        System.out.printf("%s|%s|%s|%s|$%.2f%n",
+                                deposit.getDate(), deposit.getTime(), deposit.getDescription(),deposit.getVendor(),deposit.getAmount());
+                    }
+                }
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    public static void displayReportScreen(Scanner myScanner){
+        
+
+
+
+
+    }
+
+
+
+
+
+
 
 
 
